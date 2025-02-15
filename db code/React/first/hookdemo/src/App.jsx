976cc7d16e384c06729b9a883ef4aa360ad854d2
Aizen063 <vr808187@gmail.com>
2025-02-15 11:01:13 +0530

    new change

--- db code/React/first/hookdemo/src/App.jsx
+++ db code/React/first/hookdemo/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { set } from 'mongoose'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,12 +10,24 @@ function App() {
   return (
     <>
             <h1>Count: {count}</h1>
-            {/* <h1>Decress: {count}</h1> */}
             <button onClick={() => setCount(count + 1)}>Increment</button><br></br><hr></hr>
-            <button onClick={() => setCount(count -1)}>Decress</button><br></br><hr></hr>
+            <button onClick={() => {
+              if(count==0){
+                setCount(0)
+              }
+              else{
+                setCount(count - 1)
+              }
+            }}>Decrement</button><br></br><hr></hr>
             <button onClick={() => setCount(0)}>reset</button><br></br><hr></hr>
             <button onClick={() => setCount(count + 5)}>5 Increment</button><br></br><hr></hr>
-            <button onClick={() => setCount(count - 5)}>5 dencrement</button><br></br><hr></hr>
+            <button onClick={() => {
+            if(count<=5){
+                setCount(0)
+              }
+              else{
+                setCount(count - 5)
+              }}}>5 dencrement</button><br></br><hr></hr>
     </>
   )
 }
